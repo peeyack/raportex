06ebc5caf5f7cc3cdfb99e78c061798a58ef41c3
kampiw <ka_piw@o2.pl>
2017-07-01 13:47:09 +0200

    nowy push

---
+++ raportex/src/main/java/pl/agh/edu/raportex/App.java
@@ -0,0 +1,13 @@
+package pl.agh.edu.raportex;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
