e06784b77b127122f4ee87071cadd259057de5b3
Wojciech Niziołek <wniziolek@gmail.com>
2017-07-01 15:41:12 +0200

    treeExplorer

--- raportex/src/main/java/pl/agh/edu/raportex/App.java
+++ raportex/src/main/java/pl/agh/edu/raportex/App.java
@@ -8,8 +8,11 @@ public class App
 {
     public static void main( String[] args )
     {
-    	UserInput ui = new UserInput();
+  /*  	UserInput ui = new UserInput();
 		ui.readPath("v0.1");
-		int choice = ui.readReportChoice();
+		int choice = ui.readReportChoice();*/
+		
+/*		TreeExplorer zxc = new TreeExplorer("C:\\Users\\student\\Desktop\\raportex\\raportex\\src\\test\\resources\\reporter-dane\\reporter-dane poprawne\\2012\\01");
+		zxc.test();*/
     }
 }
--- raportex/src/main/java/pl/agh/edu/raportex/TreeExplorer.java
+++ raportex/src/main/java/pl/agh/edu/raportex/TreeExplorer.java
@@ -1,5 +1,30 @@
 package pl.agh.edu.raportex;
 
-public class TreeExplorer {
+import java.io.File;
+import java.util.ArrayList;
 
+public class TreeExplorer {
+	ArrayList<String> paths = new ArrayList<String>();
+	String startPoint;
+	File folder;
+
+	
+	public TreeExplorer(String startPoint) {
+		this.startPoint = startPoint;
+		this.folder = new File(startPoint);
+		
+	}
+	
+	
+	File[] listOfFiles = folder.listFiles();
+	
+	void test() {
+		for (File file : listOfFiles) {
+		    System.out.println(file.getPath());
+		}
+		
+		
+	}
+		
+	
 }
