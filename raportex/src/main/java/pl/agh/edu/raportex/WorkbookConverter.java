8fadc2207ca8373725a53636d0e7de4b88f5eb9a
student <student@10.115.68.135>
2017-07-01 15:29:57 +0200

    dodane WBConverter

--- raportex/src/main/java/pl/agh/edu/raportex/WorkbookConverter.java
+++ raportex/src/main/java/pl/agh/edu/raportex/WorkbookConverter.java
@@ -16,31 +16,33 @@ import org.apache.poi.ss.usermodel.Workbook;
 
 public class WorkbookConverter {
 	public static ArrayList<Record> wezDaneZPlikow(ArrayList<String> sciezki) throws IOException {
-		ArrayList<Record> dane=new ArrayList<Record>();
-		for (String sciezka:sciezki) {
+		ArrayList<Record> dane = new ArrayList<Record>();
+		for (String sciezka : sciezki) {
 			dane.addAll(wezDaneZPliku(sciezka));
 		}
 		return dane;
 	}
-	
+
 	public static ArrayList<Record> wezDaneZPliku(String pathname) throws IOException {
-		HSSFWorkbook wb=new HSSFWorkbook(new FileInputStream(pathname));
-		File f=new File(pathname);
-		File p=new File(f.getAbsoluteFile().getParent());
-		File pp=new File(p.getAbsoluteFile().getParent());
-		String rok=pp.getName();
-		String mies=p.getName();
-		String gosciu=f.getName();
-		ArrayList<Record> dane=new ArrayList<Record>();
-		for (int i=0;i<wb.getNumberOfSheets();i++) {
-			HSSFSheet arkusz=wb.getSheetAt(i);
-			String nazwaProjektu=arkusz.getSheetName();
-			for (int j=1;j<arkusz.getPhysicalNumberOfRows();j++) {
-				HSSFRow wiersz=arkusz.getRow(j);
-				String task=wiersz.getCell(1).getStringCellValue();
-				double thetime=wiersz.getCell(2).getNumericCellValue();
-				Record rek=new Record (rok,mies,gosciu,nazwaProjektu,task,thetime,pathname);
-				dane.add(rek);
+		HSSFWorkbook wb = new HSSFWorkbook(new FileInputStream(pathname));
+		File f = new File(pathname);
+		File p = new File(f.getAbsoluteFile().getParent());
+		File pp = new File(p.getAbsoluteFile().getParent());
+		String rok = pp.getName();
+		String mies = p.getName();
+		String gosciu = f.getName();
+		ArrayList<Record> dane = new ArrayList<Record>();
+		for (int i = 0; i < wb.getNumberOfSheets(); i++) {
+			HSSFSheet arkusz = wb.getSheetAt(i);
+			String nazwaProjektu = arkusz.getSheetName();
+			for (int j = 1; j < arkusz.getPhysicalNumberOfRows(); j++) {
+				HSSFRow wiersz = arkusz.getRow(j);
+				if (wiersz.getPhysicalNumberOfCells() > 2) {
+					String task = wiersz.getCell(1).getStringCellValue();
+					double thetime = wiersz.getCell(2).getNumericCellValue();
+					Record rek = new Record(rok, mies, gosciu, nazwaProjektu, task, thetime, pathname);
+					dane.add(rek);
+				}
 			}
 		}
 		return dane;
